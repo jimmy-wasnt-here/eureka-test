c2eef4c329080f066614154e8a42d7bad2071e5d
Jaime Torrejon <jaime.torrejon@protonmail.com>
2020-11-12 20:47:12 +0100

    Ribbon test performed. All OK.

---
+++ lab-5-word-server-custom/src/test/java/com/example/lab5wordservercustom/Lab5WordServerCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5wordservercustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5WordServerCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
