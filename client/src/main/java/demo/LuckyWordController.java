68ccf45ee4cbe62f9659e2fef9c80d392859b95e
Jaime Torrejon <jaime.torrejon@protonmail.com>
2020-11-16 15:55:29 +0100

    Added support for spring-bus

--- client/src/main/java/demo/LuckyWordController.java
+++ client/src/main/java/demo/LuckyWordController.java
@@ -1,10 +1,11 @@
 package demo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// TODO: Add @ConfigurationProperties here.
+@ConfigurationProperties(prefix="word-config")
 public class LuckyWordController {
 	 
 	String luckyWord;
